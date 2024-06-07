99fe8b3026345f2b809428b531ee9ebda205c552
Alethepata <alessia.patane@gmail.com>
2024-06-07 17:45:37 +0200

    add categories end tags

--- app.js
+++ app.js
@@ -6,9 +6,17 @@ const app = express();
 
 const routerPost = require('./routers/posts.js');
 
+const routerTags = require('./routers/tags.js');
+
+const routerCategories = require('./routers/categories.js');
+
 app.use(express.json());
 
 app.use('/posts', routerPost);
+
+app.use('/tags', routerTags);
+
+app.use('/categories', routerCategories);
 
 app.listen(port, () => {
     console.log('Server online')
