93fc826f21c7407f40252bc2ffdae879c8e6c82b
Alethepata <alessia.patane@gmail.com>
2024-06-07 23:48:29 +0200

    edit function

--- controllers/controllerCategories.js
+++ controllers/controllerCategories.js
@@ -1,6 +1,29 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const index = async (req, res) => { 
+    try {
+        const categories = await prisma.category.findMany();
+        res.status(200).json(categories)
+    } catch (error) {
+        res.status(500).json('Errore server')
+    }
+}
+
+const show = async (req, res) => { 
+    try {
+        const category = await prisma.category.findUnique({
+            where: {
+                id : parseInt(req.params.id)
+            }
+        })
+        res.status(200).json(category)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json('Errore server')
+    }
+}
+
 const create = async (req, res) => {
     try {
         const { name } = req.body;
@@ -18,6 +41,47 @@ const create = async (req, res) => {
     }
 }
 
+const update = async (req, res) => { 
+    try {
+        const { id } = req.params;
+
+        const { name } = req.body;
+
+        const data = {
+            name
+        }
+
+        const category = await prisma.category.update({
+            where: {
+                id: parseInt(id)
+            },
+            data
+        })
+
+        res.status(200).json(category)
+    } catch (error) {
+        res.status(500).json('Errore server')
+    }
+}
+
+const destroy = async (req, res) => { 
+    try {
+        const { id } = req.params;
+        const category = await prisma.category.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        res.status(200).json(`${category.name} eliminato con successo`)
+    } catch (error) {
+        res.status(500).json('Errore server')
+    }
+}
+
 module.exports = {
+    index,
+    show,
     create,
+    update,
+    destroy
 }
--- controllers/controllerTags.js
+++ controllers/controllerTags.js
@@ -1,6 +1,29 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+const index = async (req, res) => { 
+    try {
+        const tags = await prisma.tag.findMany();
+        res.status(200).json(tags)
+    } catch (error) {
+        res.status(500).json('Errore server')
+    }
+}
+
+const show = async (req, res) => { 
+    try {
+        const tag = await prisma.tag.findUnique({
+            where: {
+                id : parseInt(req.params.id)
+            }
+        })
+        res.status(200).json(tag)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json('Errore server')
+    }
+}
+
 const create = async (req, res) => {
     try {
         const { name } = req.body;
@@ -18,6 +41,49 @@ const create = async (req, res) => {
     }
 }
 
+const update = async (req, res) => { 
+    try {
+        const { id } = req.params;
+
+        const { name } = req.body;
+
+        const data = {
+            name
+        }
+
+        const tag = await prisma.tag.update({
+            where: {
+                id: parseInt(id)
+            },
+            data
+        })
+
+        res.status(200).json(tag)
+    } catch (error) {
+        res.status(500).json('Errore server')
+    }
+}
+
+const destroy = async (req, res) => { 
+    try {
+        const { id } = req.params;
+        const tag = await prisma.tag.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        res.status(200).json(`${tag.name} eliminato con successo`)
+    } catch (error) {
+        res.status(500).json('Errore server')
+    }
+}
+
+
+
 module.exports = {
+    index,
+    show,
     create,
+    update,
+    destroy
 }
